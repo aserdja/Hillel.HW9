4edcedbe8ba00f2406f1cc743f66d11b736bf7d0
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-09-06 23:08:41 +0300

    Created three different tasks

--- HW9/ThreeTasksApp/Program.cs
+++ HW9/ThreeTasksApp/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿var task1 = Task.Run(() =>
+{
+    Console.WriteLine("Task 1 completed!");
+});
+
+var task2 = Task.Run(() =>
+{
+    Console.WriteLine(1000000 + 2000000);
+    Console.WriteLine("Task 2 completed!");
+});
+
+var task3 = Task.Run(() =>
+{
+    Thread.Sleep(5000);
+    Console.WriteLine("Task 3 completed!");
+});
+
+Task.WaitAll([task1, task2, task3]);
+Console.WriteLine("Completed");
