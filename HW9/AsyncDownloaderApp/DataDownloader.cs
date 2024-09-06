45474e448ee25acef77747be678774b5fec4a12d
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-09-06 23:57:37 +0300

    Created DataDownoader class and tested him

---
+++ HW9/AsyncDownloaderApp/DataDownloader.cs
@@ -0,0 +1,16 @@
+﻿namespace AsyncDownloaderApp
+{
+	public class DataDownloader
+	{
+		public async Task DownloadDataAsync(string url, string filePath)
+		{
+			var httpClient = new HttpClient();
+			var result = await httpClient.GetAsync(url);
+
+			using (StreamWriter sw = new StreamWriter(filePath))
+			{
+                await sw.WriteAsync(result.ToString());
+			}
+		}
+	}
+}
